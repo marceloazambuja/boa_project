18945a3ba34fd8fbae6e64bdc6535327f7b0c910
Marcelo Cunha de Azambuja <marcelo.azambuja@gmail.com>
2019-07-29 19:32:13 -0300

    instead Yahoo, now Google page is load

--- test/scraping01.py
+++ test/scraping01.py
@@ -8,10 +8,10 @@ from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Firefox()
 
-browser.get('http://www.yahoo.com')
-assert 'Yahoo' in browser.title
+browser.get('http://www.google.com')
+assert 'Google' in browser.title
 
-elem = browser.find_element_by_name('p')  # Find the search box
+elem = browser.find_element_by_name('q')  # Find the search box
 elem.send_keys('seleniumhq' + Keys.RETURN)
  
 browser.quit()
